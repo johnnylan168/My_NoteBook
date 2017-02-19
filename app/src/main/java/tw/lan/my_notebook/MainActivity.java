d933c50b08eee4e5e169eeaa65e9aca284d14590
johnnylan168 <johnnylan168@gmail.com>
2017-02-19 15:32:03 +0800

    ListView顯示畫面

--- app/src/main/java/tw/lan/my_notebook/MainActivity.java
+++ app/src/main/java/tw/lan/my_notebook/MainActivity.java
@@ -2,6 +2,11 @@ package tw.lan.my_notebook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] data = {
+                "關於Android Tutorial的事情",
+                "一隻非常可愛的小狗狗!",
+                "一首非常好聽的音樂！"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
+        ListView item_list = (ListView) findViewById(R.id.item_list);
+        item_list.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
